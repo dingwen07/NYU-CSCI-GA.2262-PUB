d7b04289a66d10763fc34cbdfbb9f0256c9bd53c
Dingwen Wang <wangdingwen32@gmail.com>
2024-10-01 07:27:51 -0400

    Update Lab 2 Problem 2

---
+++ time_app/run.py
@@ -0,0 +1,17 @@
+import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/time')
+def get_current_time():
+    return str(datetime.datetime.now())
+
+app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
